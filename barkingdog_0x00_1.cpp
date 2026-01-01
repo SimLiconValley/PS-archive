446b8cde9a9a27a81a27deaee1bad7ca1a14cf42
Limax314 <thong31415@gmail.com>
2026-01-01 22:36:31 +0900

    feat(barkingdog_0x00_1.cpp): 첫 문제 코드 작성 - 다줄 주석으로 문제 작성 - 개발 환경 구성 중

---
+++ barkingdog_0x00_1.cpp
@@ -0,0 +1,26 @@
+/**
+ * @problem
+ * 10 이하의 두 자연수가 한 줄에 공백을 사이에
+ * 두고 주어진다. 두 자연수를 합한 결과를 출력하라.
+ *
+ * @input
+ * 3 6
+ *
+ * @output
+ * 9
+ */
+
+// #include <bits/stdc++.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + b;
+}
